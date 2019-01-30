76b88b6f44fb4ae097b7965d2ca6ac795d09b9fa
tangjiahao <490384166@qq.com>
2019-01-30 14:08:41 +0800

    refactor:修改配置

--- server/config/github.conf.js
+++ server/config/github.conf.js
@@ -1,8 +1,8 @@
 module.exports = {
   // github clientid
-  client_id: '400571696b8b314100fe',
+  client_id: '',
   // github client秘钥
-  client_secret: '17d734da5ecdfe22a77ff1e434f6a1c50c028a40',
+  client_secret: '',
   // 需要从github获取的字段
   scope: 'user'
 }
